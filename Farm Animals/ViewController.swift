9e5c355d258cdfb3094089b08da7dc1bd4e4b7c4
Kaycee <KayceeT2>
2021-05-20 15:40:12 -0400

    Initial Commit

---
+++ Farm Animals/ViewController.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    var animals = [#imageLiteral(resourceName: "horse"), #imageLiteral(resourceName: "Duck"), #imageLiteral(resourceName: "Chicken"), #imageLiteral(resourceName: "Camel"), #imageLiteral(resourceName: "Pig"), #imageLiteral(resourceName: "Cow"), #imageLiteral(resourceName: "Turkey")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
